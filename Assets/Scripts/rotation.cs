a4ae6ee035e9cb46f71695395fb48cad0e4c51ac
Wizard <wizard@astral-megalith>
2018-06-05 23:59:13 -0300

    + Componentes da mecânica do jogo criados

---
+++ Assets/Scripts/rotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    public float xRate;
+    public float yRate;
+    public float zRate;
+
+    private new Transform transform;
+    
+	public void Start() 
+	{
+		transform = GetComponent<Transform>();
+	}
+    
+    public void FixedUpdate()
+    {
+        transform.Rotate(xRate, yRate, zRate);
+    }
+}
