58305f11a0274cde166569eaa2f4be5885ec6dc4
Wizard <wizard@astral-megalith>
2018-06-05 23:59:13 -0300

    + Componentes da mecânica do jogo criados

---
+++ Assets/Scripts/orbit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class orbit : MonoBehaviour
+{
+    private Vector3 direction;
+    public float speed;
+    public float deviationFromSkyline;
+    public bool clockwise;
+    public GameObject center;
+
+    private new Transform transform;
+    
+	public void Start() 
+	{
+	    direction = new Vector3(deviationFromSkyline, 0.0f, 0.0f);
+	    direction += (clockwise) ? Vector3.up : Vector3.down;
+		transform = GetComponent<Transform>();
+	}
+    
+    public void FixedUpdate()
+    {
+        transform.RotateAround (center.transform.position, 
+                                direction, 
+                                (speed * Time.deltaTime));
+    }
+}
