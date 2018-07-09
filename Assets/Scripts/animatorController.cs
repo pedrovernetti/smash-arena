9a0eb6c2ac180df0b3892ca1f4216568cf5eda03
Pedro Halmenschlager <phalmenschlager@inf.ufpel.edu.br>
2018-07-09 03:33:36 -0300

    animação corrigida, icones e imagens de seleção, embelezamento da fase fantasy

--- Assets/Scripts/animatorController.cs
+++ Assets/Scripts/animatorController.cs
@@ -15,29 +15,65 @@ public class animatorController : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.Play("Running");
             anim.SetBool("forward", true);
-        if (Input.GetKeyDown(KeyCode.S))
-            anim.SetBool("backward", true);
-        if (Input.GetKeyDown(KeyCode.A))
-            anim.SetBool("left", true);
-        if (Input.GetKeyDown(KeyCode.D))
-            anim.SetBool("right", true);
-        if (Input.GetKeyDown(KeyCode.K))
+           
+        }
+        if (Input.GetKeyDown(KeyCode.K) && anim.GetBool("forward"))
+        {
+          //  anim.Play("Dash");
             anim.SetBool("dash", true);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            anim.Play("RunningBackward");
+            anim.SetBool("backward", true);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.Play("Left");
+            anim.SetBool("left", true);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.Play("Right");
+            anim.SetBool("right", true);
+        }
+        
         if (Input.GetKeyUp(KeyCode.W))
+        {
             anim.SetBool("forward", false);
+        }
         if (Input.GetKeyUp(KeyCode.S))
+        {
             anim.SetBool("backward", false);
+        }
         if (Input.GetKeyUp(KeyCode.A))
+        {
             anim.SetBool("left", false);
+            anim.Play("Idle");
+        }
         if (Input.GetKeyUp(KeyCode.D))
+        {
             anim.SetBool("right", false);
+            anim.Play("Idle");
+        }
         if (Input.GetKeyUp(KeyCode.K))
+        {
             anim.SetBool("dash", false);
-        if (Input.GetKeyDown(KeyCode.J))
-            anim.SetBool("capoeira", true);
+            if(anim.GetBool("forward"))
+                anim.Play("Running");
+        }
+        if (Input.GetKeyDown(KeyCode.J)&& anim.GetBool("forward")==false && anim.GetBool("backward")==false)
+        {
+            anim.Play("Secret");
+            anim.SetBool("secret", true);
+        }
         if (Input.GetKeyUp(KeyCode.J))
-            anim.SetBool("capoeira", false);
+        {
+            anim.SetBool("secret", false);
+        }
 
     }
 }
